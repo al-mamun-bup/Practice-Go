a961ea39f9fd8300cb05034155f3523c684a6b82
Abdullah Al Mamun <connect.almamun.bup@gmail.com>
2025-01-19 03:29:55 +0600

    var updated

--- variables and constants/variables.go
+++ variables and constants/variables.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 	fmt.Printf("Welcome to  %v booking application\n", conferenceName)
 	fmt.Printf("We have total of  %v tickets and  %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// asking user for their name
+	userName = "Mamun"
+	userTickets = 4
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
+	// type printing
+	fmt.Printf("conference ticket is %T", conferenceTickets)
 }
