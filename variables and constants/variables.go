792f7ef780d7cb7294ba39e987b803eb35f0d630
Abdullah Al Mamun <connect.almamun.bup@gmail.com>
2025-01-19 10:46:27 +0600

    user input done

--- variables and constants/variables.go
+++ variables and constants/variables.go
@@ -1,23 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	fmt.Printf("Welcome to  %v booking application\n", conferenceName)
-	fmt.Printf("We have total of  %v tickets and  %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-
-	var userName string
-	var userTickets int
-	// asking user for their name
-	userName = "Mamun"
-	userTickets = 4
-
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
-
-	// type printing
-	fmt.Printf("conference ticket is %T", conferenceTickets)
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+	fmt.Printf("Hello, %s!\n", name)
 }
